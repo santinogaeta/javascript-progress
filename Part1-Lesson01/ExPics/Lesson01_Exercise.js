87f4a2117ded2961ca6c0cd4fc69890f9597b85d
santinogaeta <santinogaeta4@gmail.com>
2023-07-24 18:02:04 +1200

    Complete Lesson01's Exercises

---
+++ Part1-Lesson01/ExPics/Lesson01_Exercise.js
@@ -0,0 +1,19 @@
+alert('Good Morning!');
+
+alert('Santino');
+
+10 + 5
+
+20 - 5
+
+2 + 2 - 5
+
+document.body.innerHTML = 'Good Morning!';
+
+document.body.innerHTML = 'Santino';
+
+10 + 8 + 20
+
+100 - 20 - 50 + 200
+
+document.body.innerHTML = '';
